672739e6fdf6794e51f93ec6768ab807b8cfaca4
artufaro <artolfi@gmail.com>
2018-06-14 15:41:54 +0200

    fixed settings

--- settings-template.py
+++ settings-template.py
@@ -3,21 +3,20 @@
 import os
 
 #basedir = r"c:/fit/siscam"
-#basedir = "/home/gabriele/Desktop/CAMERA/CAM/"
-basedir = "/home/arturo/Documents/CAM"
+basedir = "/home/gabriele/Desktop/CAMERA/CAM/"
 
 ### Full path to image file
 #imagefile = os.path.join(basedir, 'img/test.sis')
 
 #Imaging PC writing on host
-imagefile = os.path.join(basedir, 'test.sis')
+imagefile = os.path.join('/home/gabriele/sis-fish/', 'test_TRY.sis')
 
 #Reading from imaging PC
 #imagefile = os.path.join('sftp://bec1@10.194.32.22/home/bec1/sis-fish', 'test.sis')
 
 ### Full path to incoming image-files
-watchedfiles = [os.path.join(basedir, 'test_0.sis'),
-                os.path.join(basedir, 'test_1.sis')]
+watchedfiles = [os.path.join('/home/gabriele/sis-fish/', 'test_0.sis'),
+                os.path.join('/home/gabriele/sis-fish/', 'test_1.sis')]
 
 #are they now deprecated?
 rawimage1file = imagefile
